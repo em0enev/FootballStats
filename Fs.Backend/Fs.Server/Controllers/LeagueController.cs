a607870b1e962d6b2d22059324f93fc58fa9b600
Emil Enev <emil.enev@abv.bg>
2020-12-27 21:41:17 +0200

    add LeagueController

---
+++ Fs.Backend/Fs.Server/Controllers/LeagueController.cs
@@ -0,0 +1,42 @@
+﻿namespace Fs.Server.Controllers
+{
+    using Fs.Server.Data;
+    using Fs.Server.Data.Models;
+    using Fs.Server.Models.League;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    public class LeagueController : ApiController
+    {
+        private readonly FsDbContext context;
+
+        public LeagueController(FsDbContext context)
+        {
+            this.context = context;
+        }
+
+        [Authorize]
+        [Route(nameof(CreateLeague))]
+        public async Task<ActionResult<int>> CreateLeague(CreateLeagueRequestModel model)
+        {
+            var league = new League
+            {
+                Name = model.Name
+            };
+
+            await this.context
+                .Leagues
+                .AddAsync(league);
+
+            return await this.context.SaveChangesAsync();
+        }
+
+        [Authorize]
+        [Route(nameof(Test))]
+        public ActionResult Test()
+        {
+            return this.Ok("Testa bachka");
+        }
+    }
+}
---
+++ Fs.Backend/Fs.Server/Models/League/CreateLeagueRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Fs.Server.Models.League
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateLeagueRequestModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
