2c542daeedc18eb35f57b63f11ea6846ee1799e5
Emil Enev <emil.enev@abv.bg>
2020-12-29 21:59:23 +0200

    code rafactoring

---
+++ Fs.Backend/Fs.Server/Infrastructure/ConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Fs.Server.Infrastructure
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ConfigurationExtensions
+    {
+        public static string GetDefaultConnectionString(this IConfiguration configuration)
+        {
+            return configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public static ApplicationSettings GetApplicationSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            var appSettingsConfiguration = configuration.GetSection("ApplicationSettings");
+            services.Configure<ApplicationSettings>(appSettingsConfiguration);
+            var appSettings = appSettingsConfiguration.Get<ApplicationSettings>();
+
+            return appSettings;
+        }
+    }
+}
+
--- Fs.Backend/Fs.Server/Startup.cs
+++ Fs.Backend/Fs.Server/Startup.cs
@@ -1,18 +1,13 @@
 namespace Fs.Server
 {
     using Fs.Server.Data;
-    using Fs.Server.Data.Models;
     using Fs.Server.Infrastructure;
-    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.IdentityModel.Tokens;
-    using System.Text;
 
     public class Startup
     {
@@ -27,49 +22,10 @@ namespace Fs.Server
         {
             services
                 .AddDbContext<FsDbContext>(options => options
-                    .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services
-                .AddIdentity<User, IdentityRole>(options =>
-                {
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireLowercase = false;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequiredLength = 5;
-                    options.Password.RequireNonAlphanumeric = false;
-                })
-                .AddEntityFrameworkStores<FsDbContext>();
-
-            services
+                    .UseSqlServer(Configuration.GetDefaultConnectionString()))
+                .AddIdentity()
+                .AddJwtAuthentication(services.GetApplicationSettings(this.Configuration))
                 .AddControllers();
-
-            // JWT congfig
-            var appSettingsConfiguration = this.Configuration.GetSection("ApplicationSettings");
-            services
-                    .Configure<ApplicationSettings>(appSettingsConfiguration);
-
-            var appSettings = appSettingsConfiguration.Get<ApplicationSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-                .AddJwtBearer(x =>
-                {
-                    x.RequireHttpsMetadata = false;
-                    x.SaveToken = true;
-                    x.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(key),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                });
-            // Jwt conf end
-
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -79,23 +35,19 @@ namespace Fs.Server
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseCors(opt => opt
+            app.
+                UseRouting()
+                .UseCors(opt => opt
             .AllowAnyOrigin()
             .AllowAnyMethod()
-            .AllowAnyHeader()
-            );
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            .AllowAnyHeader())
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
-
-            app.ApplyMigrations();
+            })
+                .ApplyMigrations();
         }
     }
 }
